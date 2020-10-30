307333452e538d0ba8a1d5b14f0bec70e8e457fe
Vincent Udén <vincentuden@gmail.com>
2020-10-30 15:32:57 +0000

    Modified acs

--- public/acs.sh
+++ public/acs.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
-pacman -S --noconfirm git vim xf86-video-fbdev xorg xorg-init nitrogen picom firefox base-devel xonsh
+if [[ $EUID > 0 ]]
+    then echo "Please run with sudo"
+    exit
+fi
 
-mkdir -p ~/github
-git clone https://github.com/vincent-uden/dwm ~/github/dwm
-cd ~/github/dwm
+INSTALL_USER=$SUDO_USER
+INSTALL_HOME="$(eval echo ~$INSTALL_USER)"
+
+pacman -S --noconfirm git vim xf86-video-fbdev xorg xorg-xinit nitrogen picom firefox base-devel xonsh
+
+mkdir -p "$INSTALL_HOME/github"
+
+# dwm
+git clone https://github.com/vincent-uden/dwm "$INSTALL_HOME/github/dwm"
+cd "$INSTALL_HOME/github/dwm"
 make install
+
+# st
+git clone https://github.com/vincent-uden/st "$INSTALL_HOME/github/st"
+cd "$INSTALL_HOME/github/st"
+make install
+
+# xinitrc
+curl -S http://vincentuden.xyz/xinitrc > "$INSTALL_HOME/.xinitrc"
+curl -S http://vincentuden.xyz/bash_profile > "$INSTALL_HOME/.bash_profile"
